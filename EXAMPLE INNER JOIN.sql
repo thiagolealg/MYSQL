e7159c12134e1f713fa418d31412a99ef9fa715b
thiagolealg <113521516+thiagolealg@users.noreply.github.com>
2023-01-17 13:56:23 -0300

    Update EXAMPLE INNER JOIN.sql

--- EXAMPLE INNER JOIN.sql
+++ EXAMPLE INNER JOIN.sql
@@ -1,3 +1,3 @@
 
-SELECT distinct A.CPF, A.NOME, B.CPF FROM TABELA_DE_CLIENTES A
+SELECT distinct A."campo inicial", A."campo em conjunto", B."campo a ser comparado" FROM TABELA_DE_CLIENTES A
 INNER JOIN NOTAS_FISCAIS B ON A.CPF = B.CPF;
